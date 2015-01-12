74e7fdab2525d84434b67c94a29e0a5379182cc8
NavidAnjum <navids92@gmail.com>
2015-01-12 13:55:37 +0600

    login form changed

--- Views/login.php
+++ Views/login.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,26 +11,24 @@
 </head>
 <body>
 <div class="container">
-
-<div class="row">
-
-		<div class="col-sm-12">
-		<h1>Hello</h1>
-		</div>
-		</div>
+		
 	<div class="row">
 
-		<aside class="col-sm-6" align="center">
-			<p>
-			<input type="text" name="name" placeholder="Email" style="width:300px"></p>
+		<aside class="col-sm-offset-4 col-sm-4 bg-success" >
+			
+				<h1 align="center" Style="color:Red">User Login</h1>
+
+			<p><input type="text" class="form-control" name="name" placeholder="Email" style="width:350px"></p>
 	
 			
-		<p>	<input type="password" name="pass" placeholder="password" style="width:300px">
-	
-   			<input type="submit" value="submit"></p>
+		<p>	<input type="password" class="form-control" name="pass" placeholder="password" style="width:350px">
+			</p>
+			<p><label><input type="checkbox"> Remember Me</label></p>
+			<p>
+   			<button type="button" class="btn btn-primary">Login</button></p>
     	</aside>
-    	<article class="col-s-6">
-    	<p style="color:Red">This is A new Website about learning !!</p>
+    	<article class="col-s-4">
+    	<p style="color:Red"></p>
     	</article>
 
     </div>
