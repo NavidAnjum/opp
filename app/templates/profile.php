1bf94d2edfad0395435f7635d635a6f333960205
NavidAnjum <navids92@gmail.com>
2015-01-19 14:13:37 +0600

    profile.php

--- app/templates/profile.php
+++ app/templates/profile.php
@@ -5,69 +5,115 @@
 	
 	</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/custom.css" rel="stylesheet">
+	<link href="css/profile.css" rel="stylesheet">
 	</head>
 
 	<body>
-	<div class="container-fluid container-custom" >
-		
 
-		<nav class="navbar navbar-default navbar-custom">
+	<div class="container-fluid" >
 			
-				
-				<div class="row">
-					<div class="col-md-2 navbar-header" style="font-size:30px">
-					<p style="padding-left:50px">Socialbook</p>
-					</div>
-						<div class="col-md-4">
+				<div class="row custom_header">
+
+							<div class="col-md-2">
+
+							<p>Socialbook</p>
+							
+							</div>
+
+							<div class="col-md-5 ">
 					
 						
-							<form class="navbar" role="search">
+								<form class="form-inline" role="search">
 							
-							<div class="form-group">
-							<input type="text" class="form-control" id="search" placeholder="Search for friend!">
+								<div class="form-group">
+
+								<input type="text" class="form-control" id="search" size="70px" placeholder="Search for friend!">
+
+								
+								</div>
+								
+								</form>
+
 							</div>
-						</div>
-							<p class="col-md-2">
-							<button type="submit" class="btn btn-primary">Search</button></p>
-							
-					
-						</form>
-							
-							<div class="col-md-4" style="padding-top:30px" >
-							<button type="button" class="btn btn-warning"></button>
+
+							<div class="col-md-5">
+								
 							</div>
+							
+							
 						
 						
 					</div>
 
 		
-		</nav>
-		<div class="container">
+		
+		
 	
-		<h1 align="center">Complete your profile!</h1>
-			
+		<div class="main_body">
+
 			<div class="row">
 			
-				<div class="col-md-4">
+				<div class="col-md-2 menu">
+					<h4>Picture</h4>
+					<p>Name</p>
+					<br>
+					<h4>News Feed</h4>
+
+					<p>Messages</p>
+					<p>Events</p>
+					<br>
+					<h4>Pages</h4>
+					<h4>Groups</h4>
+				
 
 				</div>
 
-				<div class="col-md-4">
-			
-				<form class="form-group">
-					<p><input type="text" class="form-control" id="name" value="<?php echo "Name"?>"></p>
-					<p>	<input type="email" class="form-control" id="email" value="<?php echo "Email"?>"></p>
-					<p><input type="text" class="form-control" id="phone" value="<?php echo "Phone"?>"></p>
-					<p>	<input type="email" class="form-control" id="address" value="<?php echo "Address"?>"></p>
-					<p><input type="file" name="file[]" multiple="multiple">Upload your picture</p>
-					<p><button type="submit" class="form-control btn btn-primary">Save</button></p>
-				</form>
+				<div class="col-md-7 section">
 
-			</div> 
+				<div class="row">
+					
+				
+					<div class="col-md-3">
+					
+					</div>
+
+					<div class="col-md-7">
+					
+					<form class="form-group">
+						<p><input type="text" class="form-control" id="name" value="<?php echo "Name"?>"></p>
+						<p>	<input type="email" class="form-control" id="email" value="<?php echo "Email"?>"></p>
+						<p><input type="text" class="form-control" id="phone" value="<?php echo "Phone"?>"></p>
+						<p>	<input type="email" class="form-control" id="address" value="<?php echo "Address"?>"></p>
+						<p>	<input type="email" class="form-control" id="address" placeholder="<?php echo "Education"?>"></p>
+						<p>	<input type="email" class="form-control" id="address" placeholder="<?php echo "Job"?>"></p>
+						<p>	<input type="email" class="form-control" id="address" placeholder="<?php echo "Gender"?>"></p>
+						<p>	<input type="email" class="form-control" id="address" placeholder="<?php echo "Date of Birth"?>"></p>
+						<p><input type="file" name="file[]" multiple="multiple">Upload your picture</p>
+						<p><button type="submit" class="form-control btn btn-primary">Save</button></p>
+					</form>
+
+					</div>
+
+					<div class="col-md-2">
+						
+					</div>
+
+				</div>
+
+				</div> 
+
+				<div class="col-md-3 aside">
+				<h4>Friend lists</h4>
+				</div>
+
+			</div>
+
 		</div>
-	</div>
-</nav>
+			<footer>
+				
+			</footer>
+
+
 </div>
 			</body>
 			</html>
